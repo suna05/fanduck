beb8748c4dfd3394340559f22e86cde2c0ca0fc1
uoonleen <uoonleen@gmail.com>
2017-07-14 15:23:31 +0900

    list-ver.1

--- uoon/calendar.js
+++ uoon/calendar.js
@@ -1,13 +1,39 @@
 // Start
 var cal = new Date()
-var month = cal.getMonth()
-_('#calendar').innerHTML = calendar(month);
+var month = cal.getMonth(),
+    year = cal.getFullYear(),
+    weekday = cal.getDay() // weekday
 
-$('.prevBtn').click(function() {
+_('#calendar').innerHTML = calendar(month, year, weekday);
 
+$(document).on('click', '.prevBtn', function() {
+  month = month - 1
+  if (month < 0) {
+    year = year - 1
+    month = 11
+  }
   _('#calendar').innerHTML = ''
-  _('#calendar').innerHTML = calendar(--month)
-  return month;
+  _('#calendar').innerHTML = calendar(month, year, weekday)
+
+  if (month != cal.getMonth() || year != (cal.getYear()+1900)) {
+    $('.week_event').css('color', 'lightgray');
+  }
+})
+
+$(document).on('click', '.nextBtn', function() {
+  month = month + 1
+  if (month > 11) {
+    year = year + 1
+    month = 0
+  }
+  _('#calendar').innerHTML = ''
+  _('#calendar').innerHTML = calendar(month, year, weekday)
+
+  if (month != cal.getMonth() || year != (cal.getYear()+1900)) {
+    $('.week_event').css('color', 'lightgray');
+  }
+    // 출력된 달이 현재 달과 일치하지 않을 경우 week_event 제거
+
 })
 
 // short queySelector
@@ -16,38 +42,38 @@ function _(s) {
 };
 
 // show info
-function showInfo(event) {
-  // link
-  var url = 'https://dl.dropboxusercontent.com/u/23834858/api/calendar.json';
-  // get json
-  getjson(url, function(obj) {
-    for (key in obj) {
-      // if has envent add class
-      if(_('[data-id="' + key + '"]')){
-        _('[data-id="' + key + '"]').classList.add('event');
-      }
-      if (event === key) {
-        // template info
-        var data = '<h3>' + obj[key].type + '</h3>' +
-            '<dl>' +
-            '<dt><dfn>Title:</dfn></dt><dd>' + obj[key].title + '</dd>' +
-            '<dt><dfn>Hour:</dfn></dt><dd>' + obj[key].time + '</dd>' +
-            '<dt><dfn>Venue:</dfn></dt><dd>' + obj[key].venue + '</dd>' +
-            '<dt><dfn>Location:</dfn></dt><dd>' + obj[key].location + '</dd>' +
-            '<dt><dfn>Description:</dfn></dt><dd>' + obj[key].desc + '</dd>' +
-            '<dt><dfn>More Info:</dfn></dt><dd><a href="' + obj[key].more +
-            '" title="More info">Here</a><dt></dd>' +
-            '</dl>';
-        return _('#calendar_data').innerHTML = data;
-      }
-    }
-  });
-}
+// function showInfo(event) {
+//   // link
+//   var url = 'https://dl.dropboxusercontent.com/u/23834858/api/calendar.json';
+//   // get json
+//   getjson(url, function(obj) {
+//     for (key in obj) {
+//       // if has envent add class
+//       if(_('[data-id="' + key + '"]')){
+//         _('[data-id="' + key + '"]').classList.add('event');
+//       }
+//       if (event === key) {
+//         // template info
+//         var data = '<h3>' + obj[key].type + '</h3>' +
+//             '<dl>' +
+//             '<dt><dfn>Title:</dfn></dt><dd>' + obj[key].title + '</dd>' +
+//             '<dt><dfn>Hour:</dfn></dt><dd>' + obj[key].time + '</dd>' +
+//             '<dt><dfn>Venue:</dfn></dt><dd>' + obj[key].venue + '</dd>' +
+//             '<dt><dfn>Location:</dfn></dt><dd>' + obj[key].location + '</dd>' +
+//             '<dt><dfn>Description:</dfn></dt><dd>' + obj[key].desc + '</dd>' +
+//             '<dt><dfn>More Info:</dfn></dt><dd><a href="' + obj[key].more +
+//             '" title="More info">Here</a><dt></dd>' +
+//             '</dl>';
+//         return _('#calendar_data').innerHTML = data;
+//       }
+//     }
+//   });
+// }
 
 // simple calendar
-function calendar(month) {
+function calendar(month, year, weekday) {
   // show info on init
-  showInfo();
+//  showInfo();
   // vars
   var day_of_week = new Array(
     'Sun', 'Mon', 'Tue',
@@ -58,31 +84,28 @@ function calendar(month) {
     'August', 'September', 'October',
     'November', 'December');
 
-  var Calendar = new Date()
-  var year = Calendar.getYear(),
+  var Calendar = new Date(),
   today = Calendar.getDate(),
-  weekday = Calendar.getDay(), // weekday
   html = '';
 
   // start in 1 and this month
   Calendar.setDate(1); // 현재 월에서 1일로 초기화한다.
+  Calendar.setYear(year);
   Calendar.setMonth(month);
 //  console.log(Calendar.getDate());
 //  console.log(Calendar.getDay()); // 2017년 7월 1일은 토요일이므로 숫자 '6'이 출력된다.
   // template calendar
-  html += '<div class="prevBtn" style="float:left; margin-left:10%; color:white">prev</div>';
-  html += '<div class="nextBtn" style="float:right; margin-right:10%; color:white">next</div>';
+  html += '<div class="prevBtn" style="float:left; margin-left:10%; color:white; cursor:pointer">prev</div>';
+  html += '<div class="nextBtn" style="float:right; margin-right:10%; color:white; cursor:pointer">next</div>';
   html += '<div class="head_cal" style="text-align:center">' + month_of_year[month] + '</div>';
   html += '<table>';
   // head
   html += '<thead>';
-  html += '<tr class="subhead_cal"><th colspan="7" style="text-align:center">' + Calendar.getFullYear() + '</th></tr>';
+  html += '<tr class="subhead_cal"><th colspan="7" style="text-align:center">' + year + '</th></tr>';
   html += '<tr class="week_cal">';
   for (index = 0; index < 7; index++) {
     if (weekday == index) {
-      if (month == Calendar.getMonth()) {
         html += '<th class="week_event" style="text-align:center">' + day_of_week[index] + '</th>';
-      }
     } else {
       html += '<th style="text-align:center">' + day_of_week[index] + '</th>';
     }
@@ -95,7 +118,7 @@ function calendar(month) {
   html += '<tr>';
   // white zone
   for (index = 0; index < Calendar.getDay(); index++) {
-    html += '<td class="white_cal"> </td>'; // 현재 달에 1일에 해당되는 요일까지를 빈칸으로 채운다.
+    html += '<td class="white_cal"> </td>'; // 현재 달에 1일에 해당되는 요일(숫자)까지를 빈칸으로 채운다.
   }
 
   for (index = 0; index < 31; index++) {
@@ -114,7 +137,7 @@ function calendar(month) {
           html += '<td><a class="today_cal" href="#" data-id="' + info + '" onclick="showInfo(\'' + info + '\')">' +
             day + '</a></td>';
 
-          showInfo(info);
+          //showInfo(info);
 
         } else {
           html += '<td><a href="#" data-id="' + info + '" onclick="showInfo(\'' + info + '\')">' +
@@ -133,19 +156,19 @@ function calendar(month) {
 }
 
 //   Get Json data
-function getjson(url, callback) {
-  var self = this,
-      ajax = new XMLHttpRequest();
-  ajax.open('GET', url, true);
-  ajax.onreadystatechange = function() {
-    if (ajax.readyState == 4) {
-      if (ajax.status == 200) {
-        var data = JSON.parse(ajax.responseText);
-        return callback(data);
-      } else {
-        console.log(ajax.status);
-      }
-    }
-  };
-  ajax.send();
-}
+// function getjson(url, callback) {
+//   var self = this,
+//       ajax = new XMLHttpRequest();
+//   ajax.open('GET', url, true);
+//   ajax.onreadystatechange = function() {
+//     if (ajax.readyState == 4) {
+//       if (ajax.status == 200) {
+//         var data = JSON.parse(ajax.responseText);
+//         return callback(data);
+//       } else {
+//         console.log(ajax.status);
+//       }
+//     }
+//   };
+//   ajax.send();
+// }
