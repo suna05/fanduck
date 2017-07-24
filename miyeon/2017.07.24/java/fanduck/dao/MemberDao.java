82f696ca5be65b5b501889cbe7152bbbe93cc3dd
miya0 <miyeon2013@gmail.com>
2017-07-24 17:03:07 +0900

    .

---
+++ miyeon/2017.07.24/java/fanduck/dao/MemberDao.java
@@ -0,0 +1,22 @@
+package fanduck.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import fanduck.domain.Member;
+
+public interface MemberDao {
+  List<Member> selectList(Map<String,Object> valueMap) ;
+ // Member selectOneByEmailPassword(Map<String,Object> valueMap);
+  int countAll();
+  int insert(Member member);
+  int delete(int mno);
+  int update(Member member);
+ 
+  void insertPhoto(Map<String,Object> valueMap);
+  String selectPhoto(int memberNo);
+  void deletePhoto(int memberNo);
+  
+  
+  List<String> selectPhotoList(int memberNo);
+}
