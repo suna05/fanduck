155680e0906961fe10151fe1dfcdab8323210033
uoonleen <uoonleen@gmail.com>
2017-08-07 20:33:02 +0900

    8월 7일 정리본

---
+++ uoonleen/8월 07일_정리/service/MoviePersonService.java
@@ -0,0 +1,21 @@
+package fanduck.service;
+
+import java.util.List;
+
+import fanduck.domain.MoviePerson;
+
+public interface MoviePersonService {
+  List<MoviePerson> list() throws Exception;
+  MoviePerson get(int mpNo) throws Exception;
+  int getSize() throws Exception;
+  void add(MoviePerson moviePerson) throws Exception;
+  void update(MoviePerson moviePerson) throws Exception;
+  void remove(int mpNo) throws Exception;
+}
+
+
+
+
+
+
+
