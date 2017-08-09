ecc704fc9c486b9d4f93ac7d7d97d63e5f579826
uoonleen <uoonleen@gmail.com>
2017-08-09 11:44:01 +0900

    전체

---
+++ uoonleen/8월 09일/main/webapp/js/userinfo.js
@@ -0,0 +1,13 @@
+var mno;
+  $.getJSON('/auth/userinfo.json', function(result) {
+        if (result.data) {
+          mno = result.data.mno
+          console.log(mno +'번 회원 <-/auth/userinfo.json통해 받아온 세션번호 at index.html')
+          detailMember(mno)
+        }
+     })
+$(document.body).on('click', '#logout-link', function(event) {
+  $.getJSON('/auth/logout.json', function(result) {
+    location.href = '../../main.html'
+  })
+})
