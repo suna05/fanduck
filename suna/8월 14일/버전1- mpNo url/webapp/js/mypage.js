5920a1d342c87adb1cf1e83960bff462fbe8d60c
suna05 <wowsun01@naver.com>
2017-08-14 16:12:38 +0900

    url mpno 1차
    url mpno 1차

---
+++ suna/8월 14일/버전1- mpNo url/webapp/js/mypage.js
@@ -0,0 +1,104 @@
+var mpList = $('#mp-array'),
+    calendarAlarm = $('#calendar_data'),
+    alarmTemplateFn,
+    alarmGeneratedHTML
+    
+    
+
+  // 영화인 목록 JSON 요청
+  function moviePersonPrint(mno) {
+	  $.getJSON('../../movieperson/list.json', {'mno':mno}, function(result) {
+	    var templateFn = Handlebars.compile($('#mp-template').text())
+		var generatedHTML = templateFn(result.data) 
+		
+		mpList.html(generatedHTML)
+	  })
+	  
+	  Handlebars.registerHelper('readYN', function(ifRead, options) {
+  	  if(ifRead == false) {
+  		  Handlebars.registerHelper('openDate', function(mvDay, options) {
+  		    if ($('.head_cal').text().length - 1 < 2) { 
+  			  if((mvDay.substring(7, 8) == $('.head_cal').text().substring(0,1)) 
+  				  && mvDay.substring(2, 4) == $('.cal_year').text().substring(2, 4)) {
+  			    return options.fn(this);
+  			  }
+  			  return options.inverse(this);
+  		  	} else {
+  		  		if((mvDay.substring(6, 8) == $('.head_cal').text().substring(0,2)) 
+  					&& mvDay.substring(2, 4) == $('.cal_year').text().substring(2, 4)) {
+  				    return options.fn(this);
+  				}
+  			  return options.inverse(this);		
+  		  	}
+  		  });  
+  	    return options.fn(this);
+  	  }
+  	  return options.inverse(this);
+    });  
+	 
+	  // 출연진 목록 JSON 요청 (알람)
+	  $.getJSON('../../castmember/list.json', {'mno':mno}, function(result) {
+		  var movieDay = result.data.list,
+		  	  getMovieDay = []
+	  	
+	  	  alarmTemplateFn = Handlebars.compile($('#alarm-template').text())
+	      alarmGeneratedHTML = alarmTemplateFn(result.data) 
+	      alarmTmp = $('#alarm-template').text()
+	      calendarAlarm.html(alarmGeneratedHTML)
+		  
+		  for (var i = 0; i < movieDay.length; i++) {
+			  getMovieDay.push(parseInt(movieDay[i].mvDay.split('월')[1].substring(1,3)))
+		  }
+		  
+		  // 현재 달이 영화의 개봉 달과 일치할 때 이벤트 발생
+		  for (var j = 0; j < getMovieDay.length; j++ ) {
+			  
+		      if(parseInt(movieDay[j].mvDay.split('년')[1].substring(1,3)) == $('.head_cal').text().split('월')[0]
+		         && movieDay[j].mvDay.split('년')[0].substring(0,4) == $('.cal_year').text()) {
+				  $('.' + getMovieDay[j]).css('background-color', 'darkseagreen').css('color', 'white');
+		      }
+		  }
+	  })
+	  
+	  
+	  
+	  // 이월 시에 알람 리로드
+	  $(document).on('click', '.moveBtn', function() {
+		  
+		$.getJSON('../../castmember/list.json', {'mno':mno}, function(result) {
+			var castMemberList2 = result.data.list
+			var alarmGeneratedHTML2 = alarmTemplateFn(result.data) 
+			var movieDay = result.data.list,
+			    getMovieDay = []
+			
+			calendarAlarm.html(alarmGeneratedHTML2)
+			
+			for (var i = 0; i < movieDay.length; i++) {
+				getMovieDay.push(parseInt(movieDay[i].mvDay.split('월')[1].substring(1,3)))
+			}
+			  
+			// 현재 달이 영화의 개봉 달과 일치할 때 이벤트 발생
+			for (var j = 0; j < getMovieDay.length; j++ ) {
+			    if(parseInt(movieDay[j].mvDay.split('년')[1].substring(1,3)) == $('.head_cal').text().split('월')[0]
+			       && movieDay[j].mvDay.split('년')[0].substring(0,4) == $('.cal_year').text()) {
+				 $('.' + getMovieDay[j]).css('background-color', 'darkseagreen').css('color', 'white');
+			    }
+			}
+		})
+	  })
+  }
+
+
+  var no = 0
+	try {
+	  no = location.href.split('?')[1].split('=')[1]
+	} catch (err) {}
+
+  $(document.body).on('click', '.detail-link', function(event) {
+	  location.href = '../board/board.html?no=' + $(this).attr('data-no') 
+	  event.preventDefault()
+  })
+  
+  
+  
+  
---
+++ suna/8월 14일/버전1- mpNo url/webapp/js/view.js
@@ -0,0 +1,349 @@
+var viewTags = $('.bit-view'),
+		newTags = $('.bit-new'),
+		//fiNo = $('#fi-no'),
+		fiBdNo = $('#fi-bdNo'),
+		fiMpNo = $('#fi-mpNo'),
+		fiTitle = $('#fi-title'),
+		fiDate = $('#fi-date'),
+		fiPhoto = $('#represent-photo'),
+		fiPhotoName = $('#represent-filename'),
+		fiContent = $('#fi-content'),
+		//fiFilenames = $('#fi-filenames');
+		fiRegister = $('#fi-rdt'),
+		fiFilenames = $('#fi-filenames'),
+		//fiPhoto = $('#fi-photo'),
+		searchValue = $('#value'),
+		searchBtn = $('#btn-search'),
+		representPhoto = $('#representPhoto')
+		photo = $('#photo'),
+		photo1 = $('#photo1'),
+		photo2 = $('#photo2'),
+		photo3 = $('#photo3'),
+		photoGallay = $('#view-photo'),
+		tbody = $('#board-ex'),
+		tbodyy = $('#board-exx'),
+		searchValue = $('#value');
+
+var mpNo = 0
+
+try {
+	mpNo = location.href.split('?')[1].split('=')[1]
+} catch (err) {}
+
+
+/*$(document.body).on('click', '.detail-link', function(event) {
+	location.href='board.html?mpNo=' + $(this).attr('data-mpNo')
+	event.preventDefault()
+})*/
+
+$(document.body).on('click', '.write-link', function(event) {
+	location.href='view.html?mpNo=' + $('.row').attr('data-mpNo')
+	event.preventDefault()
+})
+
+
+//////////////////////////////////////////////////////////////
+var bdNo = 0
+
+try {
+	mpNo = location.href.split('?')[1].split('=')[1].split('&')[0]
+	bdNo = location.href.split('&')[1].split('=')[1]
+} catch (err) {}
+
+$(document.body).on('click', '.detail-link', function(event) {
+	location.href='view.html?mpNo=' + $(this).attr('data-mpNo') + '&bdNo=' + $(this).attr('data-no') 
+	event.preventDefault()
+})
+
+
+
+
+
+$('#btn-search').click(function() {
+	console.log("검색버튼 눌렀네")
+	
+	
+	displayList2()
+	console.log(searchValue.val())
+})
+
+function displayList2() {
+$.getJSON('/board/searchList.json', {
+		'value' : '%' + searchValue.val() + '%'
+	}, function(result) {
+		console.log(result)
+		tbody.text('')
+		var totalCount = result.data.totalCount;
+
+  // 템플릿 소스를 가지고 템플릿을 처리할 함수를 얻는다.
+  var templateFn = Handlebars.compile($('#board-template').text())
+  var generatedHTML = templateFn(result.data) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
+
+  tbodyy.text('') // tbody의 기존 tr 태그들을 지우고
+  tbodyy.html(generatedHTML) // 새 tr 태그들로 설정한다.
+  
+  
+	})
+}
+
+
+
+
+
+
+
+
+
+
+//var mpNo = 0
+
+/*try {
+	mpNo = location.href.split('?')[1].split('=')[1]
+} catch (err) {}
+*/
+/*$(document.body).on('click', '.detail-link', function(event) {
+	location.href='view.html?mpNo=' + $(this).attr('data-no')
+	event.preventDefault()
+})*/
+
+
+
+
+if (bdNo == 0) { // 새 게시글 등록
+	viewTags.css('display', 'none')
+$('#add-btn').unbind('click').on('click', function(e) {
+		/*console.log(fiFilenames.val())
+		if(fiFilenames.val() == null) {
+  		filenames == "default";
+  	}*/
+	console.log(fiMpNo.val())
+		$.post('/board/add.json', {
+			//'bdNo' : fiBdNo.val(),
+			'mpNo' : mpNo,
+//			'mpNo' : "9",
+			'bdTitle': fiTitle.val(),
+			'bdContent': fiContent.val(),
+			'bdRegister': fiDate.val(),
+			'bdPhoto' : fiPhotoName.val(),
+			'filenames': fiFilenames.val()
+		}, function(result) {
+			//console.log(fiFilenames.val().filename)
+			console.log(result)
+			console.log(mpNo)
+			//console.log("===>", fiPhotoName.val())
+			location.href = "board.html?mpno=" + mpNo;
+		}, 'json')
+	})
+} else { // 게시글 정보 조회
+
+	newTags.css('display', 'none')
+
+	$.getJSON('/board/detail.json', {'bdNo': bdNo}, function(result) {
+	var data = result.data
+	fiBdNo.text(data.bdNo)
+	fiMpNo.text(data.mpNo)
+	fiTitle.val(data.bdTitle)
+	fiContent.val(data.bdContent)
+	fiDate.val(data.bdRegister)
+	fiPhotoName.val(data.bdPhoto)
+	console.log("---", data.bdPhoto)
+	representPhoto.attr("src", "../../upload/" + data.bdPhoto + ".png")
+	
+	fiFilenames.val(data.photoList)
+	//console.log("data[0]:", data.photoList[0])
+	//console.log("data[1]:", data.photoList[1])
+	//console.log(data.photoList.length)
+
+	
+	displayUpload(1)
+
+})
+
+$('#list-btn').unbind('click').on('click', function(e) {
+	//console.log("ddddd")
+	location.href = "board.html?mpno=" + mpNo;
+})
+
+
+	$('#upd-btn').unbind('click').on('click', function(e) {
+//		console.log(bdNo)
+		console.log(fiFilenames.val())
+		$.post('/board/update.json', {
+			'bdNo': bdNo,
+			'bdTitle': fiTitle.val(),
+			'bdContent': fiContent.val(),
+			'bdRegister': fiDate.val(),
+			'bdPhoto' : fiPhotoName.val(),
+			'filenames': fiFilenames.val(),
+			'photoList' : fiFilenames.val()
+			
+			
+			
+		}, function(result) {
+			console.log(result)
+			console.log(fiFilenames.val())
+			//location.href = 'board.html'
+		}, 'json')
+	})
+
+	$('#del-btn').unbind('click').on('click', function(e) {
+		$.getJSON('/board/delete.json', {'bdNo': bdNo}, function(result) {
+			location.href = 'board.html'
+		})
+	})
+
+
+}
+
+
+//$('#fi-photoupload').fileupload({
+//url: 'upload.json',
+//dataType: 'json',
+//sequentialUploads: true,  // 여러 개의 파일을 업로드 할 때 순서대로 요청하기.
+//singleFileUploads: false, // 한 요청에 여러 개의 파일을 전송시키기.  
+//done: function (e, data) { 
+//console.log('done()...');
+//console.log(data.result); // 서버가 보낸 JSON 객체는 data.result 에 보관되어 있다.
+
+//var filenames = data.result.fileList;
+
+//for (var i = 0; i <filenames.length-1 ; i++) {
+//var val = fiFilenames.val();
+//if (val.length > 0) val += ",";
+//console.log('sssss')
+//fiFilenames.val(val + filenames[i]);
+//}
+//}
+//});
+
+
+
+
+
+
+
+
+
+//핸들바스 - detail창에서 photoList 뜨게함.
+function displayUpload() {
+  // 서버에서 사진 목록 데이터를 받아 온다.
+  $.getJSON('/board/detail.json',{'bdNo': bdNo},function(result) {
+  console.log(result)
+  
+  var data = result.data.photoList
+  console.log("data=>", data)
+  
+  var $target = $('#view-photo')
+  var newEl = ''
+  for (let i = 0; i < data.length; i++) {
+     newEl += [
+        '<img src="/upload/'+ data[i] +'.png">'
+     ].join('')
+  }
+  $target.html(newEl)
+  
+  
+  }) // getJSON()
+} // displayUpload()
+
+
+
+$('#change').unbind('click').on('click', function(e) {
+	
+console.log("버튼 눌렀땅!!")
+console.log("000데이터", fiFilenames)
+$.getJSON('/board/detail.json',{'bdNo': bdNo}, function(result) {
+  console.log(result)
+  
+  var data = result.data.photoList
+  console.log("data=>", data)
+  
+  console.log("사진바꾸기", data)
+  
+  $('#fi-filenames').val('');
+  console.log("초기화1", data)
+ 
+  
+  $('#change').fileupload({
+  	url: '/board/upload.json',        // 서버에 요청할 URL
+  	dataType: 'json',         // 서버가 보낸 응답이 JSON임을 지정하기
+  	sequentialUploads: true,  // 여러 개의 파일을 업로드 할 때 순서대로 요청하기.
+  	singleFileUploads: false, // 한 요청에 여러 개의 파일을 전송시키기.
+  	autoUpload: true,        // 파일을 추가할 때 자동 업로딩 하지 않도록 설정.
+  	disableImageResize: /Android(?!.*Chrome)|Opera/
+  		.test(window.navigator && navigator.userAgent), // 안드로이드와 오페라 브라우저는 크기 조정 비활성 시키기
+  		previewMaxWidth: 100,   // 미리보기 이미지 너비
+  		previewMaxHeight: 100,  // 미리보기 이미지 높이 
+  		previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
+  		processalways: function(e, data) {
+  			console.log('리스트 변경@@@@()...');
+  			console.log("초기화 후", data)
+  			var imagesDiv = $('#images-div');
+  			imagesDiv.html("");
+  			for (var i = 0; i < data.files.length; i++) {
+  				try {
+  					if (data.files[i].preview.toDataURL) {
+  						$("<img>").attr('src', data.files[i].preview.toDataURL()).css('width', '100px').appendTo(imagesDiv);
+  					}
+  				} catch (err) {}
+  			}
+//  			$('#upload-btn').unbind("click");
+//  			$('#upload-btn').click(function() {
+//  				data.submit();
+//  			});
+  		}, 
+  		submit: function (e, data) { // 서버에 전송하기 직전에 호출된다.
+  			console.log(data.result);
+  		}, 
+
+  		
+  		done: function (e, data) { 
+  			console.log('done()...');
+  			console.log(data.result); // 서버가 보낸 JSON 객체는 data.result 에 보관되어 있다.
+
+  			var filenames = data.result.fileList;
+  			for (var i = 0; i <filenames.length ; i++) {
+  				var val = fiFilenames.val();
+  				if (val.length > 0) val += ",";
+  				console.log('사진----변경-----사진----변경')
+  				fiFilenames.val(val + filenames[i]);
+  				console.log(fiFilenames.val(val + filenames[i]))
+
+  			};
+  		}
+  });
+  
+  
+  }) // getJSON()
+
+
+})
+
+
+
+
+function displayList() {
+  // 서버에서 강사 목록 데이터를 받아 온다.
+	console.log(mpNo)
+  $.getJSON('/board/list.json',{'mpNo':mpNo}, function(result) {
+  console.log(result)
+  
+  var data = result.data
+  console.log("data=>", data.photoList)
+  
+	var totalCount = result.data.totalCount;
+  // 템플릿 소스를 가지고 템플릿을 처리할 함수를 얻는다.
+  var templateFn = Handlebars.compile($('#board-template').text())
+  var generatedHTML = templateFn(result.data) // 템플릿 함수에 데이터를 넣고 HTML을 생성한다.
+
+  tbody.text('') // tbody의 기존 tr 태그들을 지우고
+  tbody.html(generatedHTML) // 새 tr 태그들로 설정한다.
+
+
+  }) // getJSON()
+} // displayList()
+displayList()
+
+
+//displayUpload(1)
+
