0d910be97d466f97f5e064e758fcbaf7dda32843
uoonleen <uoonleen@gmail.com>
2017-08-16 19:28:46 +0900

    8월 16일 확장
    .

---
+++ uoonleen/8월 15일_확장프로그램/extension/content.js
@@ -0,0 +1,6 @@
+var jq = document.createElement('script');
+jq.onload = function(){};
+jq.src = "https://code.jquery.com/jquery-2.1.1.min.js";
+document.querySelector('head').appendChild(jq);
+
+alert($('#upload-btn'))
