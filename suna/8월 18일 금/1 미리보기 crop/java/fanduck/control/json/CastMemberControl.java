4a5ffeabb2f05f28c273e2c755091f71eca45c7f
suna05 <wowsun01@naver.com>
2017-08-18 12:54:42 +0900

    1
    1

---
+++ suna/8월 18일 금/1 미리보기 crop/java/fanduck/control/json/CastMemberControl.java
@@ -0,0 +1,70 @@
+package fanduck.control.json;
+
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import fanduck.domain.CastMember;
+import fanduck.service.CastMemberService;
+
+@RestController
+@RequestMapping("/web/castmember/")
+public class CastMemberControl {
+  
+  @Autowired ServletContext ctx;
+  @Autowired CastMemberService castMemberService;
+  
+  
+  @RequestMapping("list")
+  public JsonResult list(@RequestParam(value="mno") int mno) throws Exception {
+	System.out.println("castmember의 mno는 " + mno + "입니다.");
+    HashMap<String,Object> dataMap = new HashMap<>();
+    dataMap.put("list", castMemberService.list(mno));
+    System.out.println(dataMap);
+    
+    return new JsonResult(JsonResult.SUCCESS, dataMap);
+  }
+  
+  @RequestMapping(path="add")
+  public Object add(CastMember castMember) throws Exception {
+    System.out.println("서버 응답 완료!"); 
+    castMemberService.add(castMember);
+    return new JsonResult(JsonResult.SUCCESS, "ok");
+  }
+
+  
+  @RequestMapping("delete")
+  public JsonResult delete(CastMember castMember) throws Exception {
+    castMemberService.remove(castMember);
+    return new JsonResult(JsonResult.SUCCESS, "ok");
+  }
+  
+  @RequestMapping("select")
+  public JsonResult select(String mvNo, String fpCode) throws Exception {
+    HashMap<String,Object> dataMap = new HashMap<>();
+    dataMap.put("select", castMemberService.getByMvnoFpcode(mvNo, fpCode));
+    System.out.println(dataMap);
+    
+    return new JsonResult(JsonResult.SUCCESS, dataMap);
+  }
+  
+  @RequestMapping("update")
+  public JsonResult update(CastMember castMember) throws Exception {
+	System.out.println(castMember.isIfRead());
+    castMemberService.update(castMember);
+    return new JsonResult(JsonResult.SUCCESS, "ok");
+  }
+}
+
+
+
+
+
+
+
+
---
+++ suna/8월 18일 금/1 미리보기 crop/webapp/node_modules/tinysort/vendor/iddqd/src/iddqd.signal.swipe.js
@@ -0,0 +1,15 @@
+// todo: implement
+//		/**
+//		 * Signal for simple swipe gesture.<br/>
+//		 * The callback for this signal is Function(direction,length)
+//		 * @name iddqd.signals.swipe
+//		 * @type Signal
+//		 */
+//	createSignal('swipe',function(signal){
+//			oSignals.dragstart.add(function(added,touches){
+//				console.log('dragend',added,touches); // log
+//			});
+//			oSignals.dragend.add(function(deleted,touches){
+//				console.log('dragend',deleted,touches); // log
+//			});
+//	});
