110a06e00b3f0ef5b582e876357cf89f5dad2b7e
uoonleen <uoonleen@gmail.com>
2017-08-18 18:29:14 +0900

    8월 17일 확장

---
+++ uoonleen/8월 17일_확장프로그램 before image/extension/content.js
@@ -0,0 +1,70 @@
+var uploadBtn = document.getElementById('upload-btn')
+var body = document.querySelector('body')
+var personTags = $('#person-tags'),
+    hiddenInfo = $('.chrome-hidden-info')
+
+personTags.keydown(function(event) {
+  if (event.which == 8) {
+    personTags.val('')
+  }
+})
+
+$('#person-tags').autocomplete({
+  delay: 0,
+  minLength: 1,
+  autoFocus:true,
+  source: function(request, response) {
+    $.ajax('http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=b0cac97aa508433ca9835e54ab51d7cd&itemPerPage=500', {
+      contentType: "application/json; charset=utf-8",
+      data : { input : request.term },
+      dataType: 'json',
+      success: function(getMoviePersons) {
+
+                $('#remove-btn').click(function(){
+                  personTags.val('').removeAttr('disabled')
+                  $('div').remove('.person-array')
+                })
+
+                var list = $.map(getMoviePersons.peopleListResult.peopleList, function(item) {
+
+                  if (item.peopleNm == personTags.val()) {
+                    personTags.val('')
+                    personTags.val(item.peopleNm + ' (' + item.peopleCd + ') ').prop('disabled', 'true')
+
+                    $('<div>').text(item.peopleNm + ' (' + item.filmoNames.split('|')[0] + ')')
+                    .addClass('person-array').css('width', '185px').css('margin-left', '16px').appendTo($('.person-list'))
+
+                    $('.person-array').click(function() {
+                      personTags.val(item.peopleNm + ' (' + item.peopleCd + ') ')
+                      $('div').remove('.person-array')
+                    })
+
+                    $('.chrome-hidden-info').val(item.repRoleNm + '-' + item.filmoNames + '-' + item.peopleCd)
+                  }
+
+                	// return {
+                  //       id: item.repRoleNm + '-' + item.filmoNames + '-' + item.peopleCd,
+                  //       value: item.peopleNm + ' (' + item.peopleCd + ')',
+                  //       label: item.peopleNm + ' (' + item.filmoNames.split('|')[0] + ')'
+                  //     }
+                })
+      } // success
+    }) // ajax
+  } // source
+
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('upload-btn').addEventListener('click', alertBtn)
+})
+
+function alertBtn() {
+  insertMoviePerson(personTags, hiddenInfo)
+  // alert(personTags.val())
+}
+
+function insertMoviePerson(personTags, hiddenInfo) {
+  $.post('http://localhost:8080/movieperson/test.json', function(result) {
+    alert('과연..?')
+  })
+}
---
+++ uoonleen/8월 17일_확장프로그램 before image/extension/door.js
@@ -0,0 +1,26 @@
+/*var req;
+function openURL() {
+
+  req = new XMLHttpRequest();
+  req.onreadystatechange = handleStateChange;
+  req.open('GET', 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=b0cac97aa508433ca9835e54ab51d7cd&itemPerPage=500', true);
+  req.setRequestHeader("content-Type", "application/json;charset=utf-8")
+  req.send()
+}
+function handleStateChange() {
+  if(req.readyState == 4) {
+    if(req.status == 200) {
+      alert("성공 : " + req.responseText)
+    } else {
+      alert("실패 : " + req.status)
+    }
+  }
+}
+*/
+chrome.contextMenus.create({"title" : "영화인 추가", "contexts":["all"], "onclick":openAddPerson})
+
+function openAddPerson() {
+  chrome.windows.create({url: "local.html", type: "popup", width:300, height:450, left:1580, top:100}, function() {
+    $("body").append('Test');
+  })
+}
