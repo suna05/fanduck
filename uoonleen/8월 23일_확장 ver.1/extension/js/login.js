5bfc8953c08b2b44339217dd48681bb76e550296
uoonleen <uoonleen@gmail.com>
2017-08-23 19:23:53 +0900

    8월 23일
    확장프로그램 ver.1

---
+++ uoonleen/8월 23일_확장 ver.1/extension/js/login.js
@@ -0,0 +1,34 @@
+// send-login-info to server
+
+var fiId = $('#fi-id'),
+    fiPassword = $('#fi-password'),
+    fiIdAdd = $('#fi-id-add');
+
+$(document).on('ready', function(){
+  $('#login-btn').on('click', loginPerson)
+})
+
+function loginPerson() {
+  console.log("ak")
+  $.post('http://localhost:8080/auth/login.json', {
+    'id': fiId.val(),
+    'password': fiPassword.val()
+  }, function(result) {
+    alert(result)
+    if (result.data == 'ok') {
+      alert(result.data)
+
+   } else {
+	   alert('ID와 비밀번호가 일치하지 않습니다.')
+   }
+  }, 'json')
+  chrome.browserAction.setPopup({popup: "toolbar.html"});
+  window.close();
+  //self.close();
+}
+
+document.querySelector('#add-btn').addEventListener('click',
+  function () {
+    var url = "http://localhost:8080/main.html";
+           chrome.tabs.create({ url: url });
+    });
