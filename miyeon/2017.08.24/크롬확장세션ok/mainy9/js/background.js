731d4bedde4811869579ccbc51c621cd279002ce
miya0 <miyeon2013@gmail.com>
2017-08-24 21:47:30 +0900

    .

---
+++ miyeon/2017.08.24/크롬확장세션ok/mainy9/js/background.js
@@ -0,0 +1,8 @@
+// chrome.contextMenus.create({
+//   title:"social toolkit",
+//   contexts: ["selection"],
+//   onclick: myfunction
+// });
+// function myfunction(selectedText) {
+//   alert(selectedText.selectionText);
+// }
