57ce435e67f7976d0f51e77f309585ea3bc6e5e8
suna05 <wowsun01@naver.com>
2017-08-28 12:47:07 +0900

    2차 수정
    2차 수정

---
+++ suna/8월 28일 월/2 사진 창 크기 수정/java/fanduck/domain/MoviePerson.java
@@ -0,0 +1,77 @@
+package fanduck.domain;
+
+public class MoviePerson{
+  int mno;
+  int mpNo;
+  String mpPhotopath;
+  String mpNickname;
+  String mpContent;
+  String mpFilmo;
+  String mpCode;
+  String mpType;
+  String searchInfo;
+  String hiddenInfo;
+  
+public int getMno() {
+	return mno;
+}
+public void setMno(int mno) {
+	this.mno = mno;
+}
+public int getMpNo() {
+	return mpNo;
+}
+public void setMpNo(int mpNo) {
+	this.mpNo = mpNo;
+}
+public String getMpPhotopath() {
+	return mpPhotopath;
+}
+public void setMpPhotopath(String mpPhotopath) {
+	this.mpPhotopath = mpPhotopath;
+}
+public String getMpNickname() {
+	return mpNickname;
+}
+public void setMpNickname(String mpNickname) {
+	this.mpNickname = mpNickname;
+}
+public String getMpContent() {
+	return mpContent;
+}
+public void setMpContent(String mpContent) {
+	this.mpContent = mpContent;
+}
+public String getMpFilmo() {
+	return mpFilmo;
+}
+public void setMpFilmo(String mpFilmo) {
+	this.mpFilmo = mpFilmo;
+}
+public String getMpCode() {
+	return mpCode;
+}
+public void setMpCode(String mpCode) {
+	this.mpCode = mpCode;
+}
+public String getMpType() {
+	return mpType;
+}
+public void setMpType(String mpType) {
+	this.mpType = mpType;
+}
+public String getSearchInfo() {
+	return searchInfo;
+}
+public void setSearchInfo(String searchInfo) {
+	this.searchInfo = searchInfo;
+}
+public String getHiddenInfo() {
+	return hiddenInfo;
+}
+public void setHiddenInfo(String hiddenInfo) {
+	this.hiddenInfo = hiddenInfo;
+}
+
+}
+  
